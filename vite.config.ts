fbe55978edd69f01237165ff42ff148d9e9bb39a
Daniel Herrera-Vasquez <26307270+dannyhvc@users.noreply.github.com>
2026-04-16 01:49:48 -0400

    bumped vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// Recreate __dirname for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
-    // Use a relative base path so assets load correctly in subdirectories (like GitHub Pages)
-    base: "./",
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
+	base: '/newtrackselfcare/',
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
